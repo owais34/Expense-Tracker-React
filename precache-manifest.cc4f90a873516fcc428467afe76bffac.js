e8dd3bfe2d607281259a57fa6c95e16d2df0b0c4
owais34 <owaishunter78@gmail.com>
2020-08-27 18:21:46 +0530

    Updates

---
+++ precache-manifest.cc4f90a873516fcc428467afe76bffac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb2e470fa563a11422f8c1d2f78a8e63",
+    "url": "/expense-tracker-react/index.html"
+  },
+  {
+    "revision": "06d99a746bb00f0ef310",
+    "url": "/expense-tracker-react/static/css/main.d0e32a8e.chunk.css"
+  },
+  {
+    "revision": "b04f4b1e00444b1e9801",
+    "url": "/expense-tracker-react/static/js/2.b93bb5d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expense-tracker-react/static/js/2.b93bb5d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06d99a746bb00f0ef310",
+    "url": "/expense-tracker-react/static/js/main.6ca8f7c7.chunk.js"
+  },
+  {
+    "revision": "27d5a3916d5c0aa966b2",
+    "url": "/expense-tracker-react/static/js/runtime-main.69aaf8cd.js"
+  }
+]);
